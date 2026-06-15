ceea2a947e20302d4ef78f0c15aefa49b8477909
Saifullah <saifullahimtiaz692@gmail.com>
2026-06-15 09:18:00 +0500

    Add files via upload

---
+++ StudentResult.java
@@ -0,0 +1,64 @@
+public class StudentResult {
+
+    private int[] marks;
+
+    public StudentResult(int[] marks) {
+
+        if (marks == null || marks.length == 0) {
+            throw new IllegalArgumentException("Marks cannot be empty");
+        }
+
+        for (int mark : marks) {
+            if (mark < 0 || mark > 100) {
+                throw new IllegalArgumentException(
+                    "Marks must be between 0 and 100"
+                );
+            }
+        }
+
+        this.marks = marks;
+    }
+
+    public int calculateTotal() {
+        int total = 0;
+
+        for (int mark : marks) {
+            total += mark;
+        }
+
+        return total;
+    }
+
+    public double calculateAverage() {
+        return (double) calculateTotal() / marks.length;
+    }
+
+    public String getResultStatus() {
+
+        for (int mark : marks) {
+            if (mark < 40) {
+                return "Fail";
+            }
+        }
+
+        return "Pass";
+    }
+
+    public String getGrade() {
+
+        double avg = calculateAverage();
+
+        if (avg >= 90)
+            return "A+";
+        else if (avg >= 80)
+            return "A";
+        else if (avg >= 70)
+            return "B";
+        else if (avg >= 60)
+            return "C";
+        else if (avg >= 50)
+            return "D";
+        else
+            return "F";
+    }
+}
